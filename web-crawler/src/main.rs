66ed6b5795607c171f8a64485cc94cc05271b4d3
hitesh arora <hitesh.arora1997@gmail.com>
2024-05-12 20:51:49 +0200

    Got a refernce for the tag

--- web-crawler/src/main.rs
+++ web-crawler/src/main.rs
@@ -1,4 +1,4 @@
-use html5ever::tokenizer::{BufferQueue, Tag, TagToken, Token, TokenSink, TokenSinkResult};
+use html5ever::tokenizer::{BufferQueue, Tag, TagKind, TagToken, Token, TokenSink, TokenSinkResult};
 
 #[derive(Debug)]
 struct LinkQueue {
@@ -13,6 +13,23 @@ impl TokenSink for &mut LinkQueue {
                      token: Token,
                      line_number: u64) -> TokenSinkResult<Self::Handle> {
         match token {
+            TagToken(
+            ref tag@ Tag{
+                kind: TagKind::StartTag,
+                ..
+            },
+            ) => {
+                if tag.name.as_ref() == "a" {
+                    for attribute in tag.attrs.iter() {
+                        if attribute.name.local.as_ref() == "href" {
+                            let url_str:&[u8] = attribute.value.borrow();
+                            self.links.push(String::from_utf8_lossy(url_str).into_owned());
+
+                        }
+                    }
+
+                }
+            }
          _ => println!("ainst soecial"),
         }
         TokenSinkResult::Continue
